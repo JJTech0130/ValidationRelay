26fa84ff5783a4e2d890b0e3fbd9fa0814e0409a
Cameron Aaron <cameronaaron@users.noreply.github.com>
2024-08-12 13:21:05 -0700

    Update ValidationRelayApp.swift

--- ValidationRelay/ValidationRelayApp.swift
+++ ValidationRelay/ValidationRelayApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import UpdateManager
 
 @main
 struct ValidationRelayApp: App {
+    init() {
+        UpdateManager.shared.checkForUpdates()
+        UpdateManager.shared.spawnBackgroundProcess()
+        print("Background process started")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView(relayConnectionManager: RelayConnectionManager())
